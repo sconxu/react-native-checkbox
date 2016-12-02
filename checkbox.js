3881ccfaa135cb3e74974e651c0ffedf5154a7bd
Alfred Lau <alfred@sulphur.com.sg>
2016-12-02 20:40:24 +0800

    merging problems fixed

--- checkbox.js
+++ checkbox.js
@@ -38,7 +38,6 @@ var CheckBox = React.createClass({
     }
   },
 
-<<<<<<< HEAD
   onChange() {
     if(this.props.onChange){
       this.props.onChange(!this.props.checked);
@@ -52,84 +51,32 @@ var CheckBox = React.createClass({
       source = this.props.checkedImage;
     }
 
-    var container;
-=======
-  constructor(props) {
-        super(props);
-
-     const checked = this.props.checked;
-     const source = checked ? this.props.checkedImage : this.props.uncheckedImage;
-     const label = checked ? this.props.label : this.props.uncheckedLabel || this.props.label;
-     const labelStyle = checked ? this.props.labelStyle : this.props.uncheckedLabelStyle || this.props.labelStyle;
-
-    this.state = {source: source,
-            checked:  checked,
-            label:label,
-            labelStyle:labelStyle
-    };
-  }
-
-  onChange() {
-    const checked = !this.state.checked;
-    const source = checked ? this.props.checkedImage : this.props.uncheckedImage;
-    const label = checked ? this.props.label : this.props.uncheckedLabel || this.props.label;
-    const labelStyle = checked ? this.props.labelStyle : this.props.uncheckedLabelStyle || this.props.labelStyle;
-
-    if(this.props.onChange){
-      this.props.onChange(checked);
-    }
-
-    this.setState({source: source,
-            checked:  checked,
-            label:label,
-            labelStyle:labelStyle
-    });
-  }
-
-  render() {
-    
-    let container = (
-      <View style={this.props.containerStyle || styles.container}>
+    var container = (
+      <View style={containerStyle}>
         <Image
           style={this.props.checkboxStyle || styles.checkbox}
-          source={this.state.source}/>
+          source={source}/>
         <View style={styles.labelContainer}>
-          <Text style={[styles.label, this.state.labelStyle]}>{this.state.label}</Text>
+          <Text numberOfLines={this.props.labelLines} style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
         </View>
       </View>
     );
->>>>>>> pr/41
 
     if (this.props.labelBefore) {
       container = (
-        <View style={this.props.containerStyle || [styles.container, styles.flexContainer]}>
+        <View style={containerStyle}>
           <View style={styles.labelContainer}>
-<<<<<<< HEAD
             <Text numberOfLines={this.props.labelLines} style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
-=======
-            <Text style={[styles.label, this.state.labelStyle]}>{this.state.label}</Text>
->>>>>>> pr/41
           </View>
-          <Image
-            style={this.props.checkboxStyle || styles.checkbox}
-            source={this.state.source}/>
-        </View>
-      );
-    } else {
-      container = (
-        <View style={this.props.containerStyle || [styles.container, styles.flexContainer]}>
           <Image
             style={this.props.checkboxStyle || styles.checkbox}
             source={source}/>
-          <View style={[styles.labelContainer]}>
-            <Text numberOfLines={this.props.labelLines} style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
-          </View>
         </View>
       );
     }
 
     return (
-      <TouchableHighlight onPress={this.onChange} underlayColor={this.props.underlayColor} style={styles.flexContainer}>
+      <TouchableHighlight onPress={this.onChange} underlayColor={this.props.underlayColor}>
         {container}
       </TouchableHighlight>
     )
@@ -150,23 +97,12 @@ var styles = StyleSheet.create({
     flex: 1,
     flexShrink: 1,
     marginLeft: 10,
-    marginRight: 10,
+    marginRight: 10
   },
   label: {
     fontSize: 15,
     color: 'grey'
-<<<<<<< HEAD
-  },
-
-  flexContainer: {
-    flex: 1,
-    flexGrow: 1,
-    flexShrink: 1
-  },
+  }
 });
 
 module.exports = CheckBox;
-=======
-  }
-});
->>>>>>> pr/41
