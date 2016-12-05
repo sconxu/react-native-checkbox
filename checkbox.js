18aa8934ed3ccaf808919fd1537b1d435117a4c4
Bogdan Carmanus <bogdan.carmanus@itnt.ro>
2016-12-05 11:32:14 +0200

    refactored checkbox to support both internal state and external state

--- checkbox.js
+++ checkbox.js
@@ -1,19 +1,112 @@
 'use strict';
 
-var React = require('react');
-var ReactNative = require('react-native');
+import React, { Component } from 'react';
 var PropTypes = React.PropTypes;
+import {
+    StyleSheet,
+    Image,
+    Text,
+    View,
+    TouchableHighlight
+} from 'react-native';
+const CB_ENABLED_IMAGE = require('./cb_enabled.png');
+const CB_DISABLED_IMAGE = require('./cb_disabled.png');
 
-var {
-  StyleSheet,
-  Image,
-  Text,
-  View,
-  TouchableHighlight
-} = ReactNative;
+class CheckBox extends Component {
+    constructor(props) {
+        super(props);
 
-var CheckBox = React.createClass({
-  propTypes: {
+        this.state = {
+            internalChecked: false
+        }
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange() {
+        if (this.props.checked && this.props.onChange) {
+            this.props.onChange();
+        } else {
+            let internalChecked = this.state.internalChecked;
+
+            this.setState({
+                internalChecked: !internalChecked
+            });
+        }
+    }
+
+    render() {
+        let container = (
+            <View style={this.props.containerStyle || styles.container}>
+                <Image
+                style={this.props.checkboxStyle || styles.checkbox}
+                source={source}/>
+                <View style={styles.labelContainer}>
+                    <Text style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
+                </View>
+            </View>
+        );
+
+        let source = this.state.internalChecked ? this.props.checkedImage : this.props.uncheckedImage;
+
+        if(this.props.checked) {
+            source = this.props.checked ? this.props.checkedImage : this.props.uncheckedImage;
+        }
+
+
+        if (this.props.labelBefore) {
+            container = (
+                <View style={this.props.containerStyle || [styles.container, styles.flexContainer]}>
+                    <View style={styles.labelContainer}>
+                        <Text numberOfLines={this.props.labelLines} style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
+                    </View>
+                    <Image
+                    style={[styles.checkbox, this.props.checkboxStyle]}
+                    source={source}/>
+                </View>
+            );
+        } else {
+            container = (
+                <View style={[styles.container, this.props.containerStyle]}>
+                    <Image
+                    style={[styles.checkbox, this.props.checkboxStyle]}
+                    source={source}/>
+                    <View style={styles.labelContainer}>
+                        <Text numberOfLines={this.props.labelLines} style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
+                    </View>
+                </View>
+            );
+        }
+
+        return (
+            <TouchableHighlight onPress={this.onChange} underlayColor={this.props.underlayColor} style={styles.flexContainer}>
+                {container}
+            </TouchableHighlight>
+        )
+    }
+};
+
+var styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 5,
+    },
+    checkbox: {
+        width: 26,
+        height: 26
+    },
+    labelContainer: {
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    label: {
+        fontSize: 15,
+        color: 'grey'
+    }
+});
+
+CheckBox.propTypes = {
     label: PropTypes.string,
     labelStyle: PropTypes.oneOfType([PropTypes.array,PropTypes.object,PropTypes.number]),
     labelLines: PropTypes.number,
@@ -24,149 +117,16 @@ var CheckBox = React.createClass({
     uncheckedImage: PropTypes.number,
     underlayColor: PropTypes.string,
     onChange: PropTypes.func
-  },
+};
 
-  getDefaultProps() {
-    return {
-      label: 'Label',
-      labelLines: 1,
-      labelBefore: false,
-      checked: false,
-      checkedImage: require('./cb_enabled.png'),
-      uncheckedImage: require('./cb_disabled.png'),
-      underlayColor: 'white'
-    }
-  },
-
-<<<<<<< HEAD
-  onChange() {
-    if(this.props.onChange){
-      this.props.onChange(!this.props.checked);
-    }
-  },
-
-  render() {
-    var source = this.props.uncheckedImage;
-
-    if(this.props.checked){
-      source = this.props.checkedImage;
-    }
-
-    var container;
-=======
-  constructor(props) {
-        super(props);
-
-     const checked = this.props.checked;
-     const source = checked ? this.props.checkedImage : this.props.uncheckedImage;
-     const label = checked ? this.props.label : this.props.uncheckedLabel || this.props.label;
-     const labelStyle = checked ? this.props.labelStyle : this.props.uncheckedLabelStyle || this.props.labelStyle;
-
-    this.state = {source: source,
-            checked:  checked,
-            label:label,
-            labelStyle:labelStyle
-    };
-  }
-
-  onChange() {
-    const checked = !this.state.checked;
-    const source = checked ? this.props.checkedImage : this.props.uncheckedImage;
-    const label = checked ? this.props.label : this.props.uncheckedLabel || this.props.label;
-    const labelStyle = checked ? this.props.labelStyle : this.props.uncheckedLabelStyle || this.props.labelStyle;
-
-    if(this.props.onChange){
-      this.props.onChange(checked);
-    }
-
-    this.setState({source: source,
-            checked:  checked,
-            label:label,
-            labelStyle:labelStyle
-    });
-  }
-
-  render() {
-    
-    let container = (
-      <View style={this.props.containerStyle || styles.container}>
-        <Image
-          style={this.props.checkboxStyle || styles.checkbox}
-          source={this.state.source}/>
-        <View style={styles.labelContainer}>
-          <Text style={[styles.label, this.state.labelStyle]}>{this.state.label}</Text>
-        </View>
-      </View>
-    );
->>>>>>> pr/41
-
-    if (this.props.labelBefore) {
-      container = (
-        <View style={this.props.containerStyle || [styles.container, styles.flexContainer]}>
-          <View style={styles.labelContainer}>
-<<<<<<< HEAD
-            <Text numberOfLines={this.props.labelLines} style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
-=======
-            <Text style={[styles.label, this.state.labelStyle]}>{this.state.label}</Text>
->>>>>>> pr/41
-          </View>
-          <Image
-            style={this.props.checkboxStyle || styles.checkbox}
-            source={this.state.source}/>
-        </View>
-      );
-    } else {
-      container = (
-        <View style={this.props.containerStyle || [styles.container, styles.flexContainer]}>
-          <Image
-            style={this.props.checkboxStyle || styles.checkbox}
-            source={source}/>
-          <View style={[styles.labelContainer]}>
-            <Text numberOfLines={this.props.labelLines} style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
-          </View>
-        </View>
-      );
-    }
-
-    return (
-      <TouchableHighlight onPress={this.onChange} underlayColor={this.props.underlayColor} style={styles.flexContainer}>
-        {container}
-      </TouchableHighlight>
-    )
-  }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 5,
-  },
-  checkbox: {
-    width: 26,
-    height: 26
-  },
-  labelContainer: {
-    flex: 1,
-    flexShrink: 1,
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  label: {
-    fontSize: 15,
-    color: 'grey'
-<<<<<<< HEAD
-  },
-
-  flexContainer: {
-    flex: 1,
-    flexGrow: 1,
-    flexShrink: 1
-  },
-});
+CheckBox.defaultProps = {
+    label: 'Label',
+    labelLines: 1,
+    labelBefore: false,
+    checked: null,
+    checkedImage: CB_ENABLED_IMAGE,
+    uncheckedImage: CB_DISABLED_IMAGE,
+    underlayColor: 'white'
+};
 
 module.exports = CheckBox;
-=======
-  }
-});
->>>>>>> pr/41
