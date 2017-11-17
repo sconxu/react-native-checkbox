64122aee59c89fa15e11e45be2e66be1e6d00276
Komaldeep Chahal <komaldeep1993@gmail.com>
2017-11-17 16:20:12 +0100

    add(test): test

--- Example/_tests_/App.test.js
+++ Example/_tests_/App.test.js
@@ -3,7 +3,7 @@ import findById from '../test/findById';
 import renderer from 'react-test-renderer';
 import App from '../App';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toMatchSnapshot();
 });
