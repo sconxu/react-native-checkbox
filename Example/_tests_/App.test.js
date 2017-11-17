6e2da50e1c9ea90a5b55a1500118d5b93121f583
Komaldeep Chahal <komaldeep1993@gmail.com>
2017-11-17 15:56:02 +0100

    add(architecture): architecture for test

--- Example/_tests_/App.test.js
+++ Example/_tests_/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import findById from './test/findById';
+import findById from '../test/findById';
 import renderer from 'react-test-renderer';
-import App from './App';
+import App from '../App';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
